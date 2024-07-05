bbd6068bb0feab46ab94331384959470c4420196
SUJAN <pokharelisujan@gmail.com>
2024-07-05 23:18:58 +0900

    test

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class DemoController {
-	//test
+	//tests
     @GetMapping("/hello")
     public String hello(Model model) {
         model.addAttribute("message", "Hello, World! , i Am  auto deploying with jenkin");
